9ee8709d728bf32c11bae7a42059a0f702363e3c
Johannes Lohner <johannes.lohner@hotmail.com>
2018-05-15 15:42:51 +0200

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # raise
+    # @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    # raise
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    # @task = Task.find(params[:id])
+  end
+
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+  #returns filtered hash
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read All
+  get "tasks", to: "tasks#index"
+
+  # CREATE
+    # Create new Task - FORM
+  get "tasks/new", to: "tasks#new", as: :newtask
+    # Post new Task
+  post "tasks", to: "tasks#create"
+
+  # Read one
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # DELETE
+  delete "tasks/:id",      to: "tasks#destroy"
+
+  # UPDATE
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edittask
+  patch  "tasks/:id",      to: "tasks#update"
+
+
+
+
 end
